40aa5d713854f3343ba1e111354e6ca5cf81a69a
TennyZhuang <zty0826@gmail.com>
2016-01-18 07:50:56 +0800

    fix: observer pattern

--- Observer.py
+++ Observer.py
@@ -1,9 +1,15 @@
-import Subject
+from Subject import Subject
+
 
 class Observer(object):
     def __init__(self, subject):
         assert isinstance(subject, Subject)
         self.subject = subject
+        self.subject.attach(self)
+
+    def __del__(self):
+        if self in self.subject.observers:
+            self.subject.detach(self)
 
     def update(self):
         raise NotImplementedError()
--- Subject.py
+++ Subject.py
@@ -1,15 +1,11 @@
-import Observer
-
 class Subject(object):
     def __init__(self):
         self.observers = []
 
     def attach(self, ob):
-        assert isinstance(ob, Observer)
         self.observers.append(ob)
 
     def detach(self, ob):
-        assert isinstance(ob, Observer)
         self.observers.remove(ob)
 
     def notify(self):
