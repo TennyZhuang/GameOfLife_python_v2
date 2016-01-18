84c18155ce0d1d386d19f27a726586f06566c211
TennyZhuang <zty0826@gmail.com>
2016-01-18 10:35:41 +0800

    add: repeatable timer

---
+++ RepeatableTimer.py
@@ -0,0 +1,23 @@
+from threading import Timer
+from Subject import Subject
+
+
+class RepeatableTimer(Subject):
+    def __init__(self, interval):
+        super().__init__()
+        self.interval = interval
+
+    def start(self):
+        self.stop()
+        self._timer = Timer(self.interval, self._run)
+        self._timer.setDaemon(True)
+        self._timer.start()
+
+    def stop(self):
+        if '_timer' in self.__dict__:
+            self._timer.cancel()
+            del self._timer
+
+    def _run(self):
+        self.notify()
+        self.start()
