79db37d0d955e338d8a768e047642239b52abccc
TennyZhuang <zty0826@gmail.com>
2016-01-19 06:10:28 +0800

    add: deconstructor for timer

--- RepeatableTimer.py
+++ RepeatableTimer.py
@@ -7,6 +7,9 @@ class RepeatableTimer(Subject):
         super().__init__()
         self.interval = interval
 
+    def __del__(self):
+        self.stop()
+
     def start(self):
         self.stop()
         self._timer = Timer(self.interval, self._run)
